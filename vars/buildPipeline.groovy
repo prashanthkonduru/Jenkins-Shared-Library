91d9890c079c5bb2e6e3ee88b689c168c8b5739b
prashanthkonduru <kpkumar.konduru@gmail.com>
2019-12-13 09:18:16 +0530

    updated

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -1,7 +1,7 @@
 def call(Map buildParam)
 {
 properties([parameters([choice(choices: ['SNAPSHOT', 'RELEASE'], description: 'Select type', name: 'BUILD')])])
-node
+node(){
 stage('Demo') 
 {
     def mvn_version = "Maven"
@@ -17,6 +17,6 @@ stage('Demo')
 	echo 'Vamsi is great'
 	}
 }
-
+}
 
 }
