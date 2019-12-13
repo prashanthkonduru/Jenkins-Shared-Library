7ca1877e679efa5ee2605272f686f886ee4ba14b
prashantkonduru <kpkumar.konduru@gmail.com>
2019-12-13 08:40:05 +0530

    updated

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -2,9 +2,10 @@ def call(Map buildParam)
 {
 properties([parameters([choice(choices: ['SNAPSHOT', 'RELEASE'], description: 'Select type', name: 'BUILD')])])
 
-}
 stage('Demo') 
 {
     echo 'Hello Vamsi'
     sayHello 'Dave'
+}
+
 }
