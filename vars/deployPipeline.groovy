d26d32189b762552dc00189ebed230d9b8e22e66
prashanthkonduru <46436114+prashanthkonduru@users.noreply.github.com>
2020-01-08 09:04:03 +0530

    Update deployPipeline.groovy

--- vars/deployPipeline.groovy
+++ vars/deployPipeline.groovy
@@ -9,11 +9,6 @@ def call()
 		   sh "scp -o StrictHostKeyChecking=no target/*.war ec2-user@35.154.170.98:/opt/apache-tomcat-9.0.30/webapps/"
 		} 
 	   
-		else
-		{
-			echo 'Not deployed'
-		}
 	}
-
 
 }
