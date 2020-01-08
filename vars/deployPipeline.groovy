d45c5ed79f866feb2cddc5977cc0a9bb512d3bed
prashanthkonduru <46436114+prashanthkonduru@users.noreply.github.com>
2020-01-08 09:08:19 +0530

    Added Node

--- vars/deployPipeline.groovy
+++ vars/deployPipeline.groovy
@@ -1,14 +1,14 @@
 def call()
 {
-
-	stage('Deploy stage')
+	node('master')
 	{
-		
-		sshagent(['9041adc5-5b08-4d24-b296-7351557864ca']) 
+		stage('Deploy stage')
 		{
-		   sh "scp -o StrictHostKeyChecking=no target/*.war ec2-user@35.154.170.98:/opt/apache-tomcat-9.0.30/webapps/"
-		} 
-	   
+			sshagent(['9041adc5-5b08-4d24-b296-7351557864ca']) 
+			{
+			   sh "scp -o StrictHostKeyChecking=no target/*.war ec2-user@35.154.170.98:/opt/apache-tomcat-9.0.30/webapps/"
+			} 
+		}
 	}
 
 }
