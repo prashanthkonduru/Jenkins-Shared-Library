8ce7e124cfd38b1ed6b7bc1cd210f0c9b943287d
prashanthkonduru <46436114+prashanthkonduru@users.noreply.github.com>
2020-01-08 09:14:26 +0530

    Updated Credentails

--- vars/deployPipeline.groovy
+++ vars/deployPipeline.groovy
@@ -4,7 +4,7 @@ def call()
 	{
 		stage('Deploy stage')
 		{
-			sshagent(['9041adc5-5b08-4d24-b296-7351557864ca']) 
+			sshagent(['Tomcat-Credentials']) 
 			{
 			   sh "scp -o StrictHostKeyChecking=no target/*.war ec2-user@35.154.170.98:/opt/apache-tomcat-9.0.30/webapps/"
 			} 
