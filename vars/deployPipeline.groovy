1ee25917912ee9307f884d20e61176f3fc451195
prashanthkonduru <46436114+prashanthkonduru@users.noreply.github.com>
2020-02-05 08:10:29 +0530

    Removed extra characters

--- vars/deployPipeline.groovy
+++ vars/deployPipeline.groovy
@@ -2,14 +2,14 @@ def call()
 {
 	node('master')
 	{
-<<<<<<< HEAD
+
 		
 		sshagent(['9041adc5-5b08-4d24-b296-7351557864ca']) 
 		{
 		   sh "scp -o StrictHostKeyChecking=no target/*.war ec2-user@35.154.170.98:/opt/apache-tomcat-9.0.30/webapps/"
 		} 
 	}
-=======
+
 		stage('Deploy stage')
 		{
 			sshagent(['Tomcat-Credentials']) 
@@ -19,5 +19,5 @@ def call()
 		}
 	}
 
->>>>>>> 8ce7e124cfd38b1ed6b7bc1cd210f0c9b943287d
+
 }
